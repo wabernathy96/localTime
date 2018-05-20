c31b0052505ae00a3bd42f3900c2fc5123f4b610
wabernathy96 <wabernathy96@gmail.com>
2018-05-20 03:41:35 -0500

    Revert "planners ARE ROLLLLLLING NOWWW"
    This reverts commit 0dda540b4772289530f4a311dbf4ff238436626f.

--- app/controllers/user_c.js
+++ app/controllers/user_c.js
@@ -38,15 +38,14 @@ module.exports = {
 
     userInfo: (req,res) => {
         console.log("here")
-        db.user.findAll({
+        db.user.findOne({
             where:{
                 userId: req.user.userId
-            },include: [{ model: db.planner, where: { userID: req.user.userId }, required: false }]
+            }
         }).then(
             (user) => {
-                console.log(user[0].dataValues.planner.dataValues.city)
-                res.render('pages/user_dash', { user : user[0].dataValues.firstName, plan: user}
-                
+                console.log(user)
+                res.render('pages/user_dash', { user : user.dataValues.firstName}
                 )
             }
         )
